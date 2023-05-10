f8f4382a09eb9253f8b770348d0f66965466aadf
souzare <renansouza.ds@gmail.com>
2023-05-10 07:03:26 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,4 +16,11 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
+    int foo(int a) {
+  int b = 12;
+  if (a == 1) {
+    return b;
+  }
+  return b;  // Noncompliant
+}
 }
